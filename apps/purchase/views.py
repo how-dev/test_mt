2ed2b1a6b69d640230bd32d302e0edabf7ba5ac7
how.dev <howard.medeiros@stormgroup.com.br>
2022-02-01 20:55:18 -0300

    feature/cache was implemented in GET cashback route

--- apps/purchase/views.py
+++ apps/purchase/views.py
@@ -1,3 +1,6 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_headers
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
@@ -32,3 +35,16 @@ class PurchaseViewSet(ModelViewSet):
             raw_data=str(cashback)
         )
         return Response(cashback, status=status.HTTP_201_CREATED)
+
+    @method_decorator(cache_page(60, key_prefix="list"))
+    @method_decorator(vary_on_headers("Authorization"))
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
